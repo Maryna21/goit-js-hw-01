08dadaf8907ccf197d43bccda4a294e9aa51488a
Maryna21 <64320384+Maryna21@users.noreply.github.com>
2020-09-08 15:25:39 +0300

    6 завдань чат-бота

---
+++ js/task-1.js
@@ -0,0 +1,4 @@
+// const name = "Генератор захисного поля";
+// const price = 2000;
+// const order = `Обрано ${name}, ціна за штуку ${price} кредитів`;
+// console.log(order);
---
+++ js/task-3.js
@@ -0,0 +1,41 @@
+// const ADMIN_PASSWORD = "jqueryismyjam";
+// let message;
+// const password = prompt("Введіть будь ласка ваш пароль");
+// if ((password = false)) {
+//   console.log((message = "Скасовано користувачем!"));
+// }
+// let message = "Скасовано користувачем!";
+// let message = `Ласкаво просимо!`;
+// let message = "Доступ заборонений, невірний пароль!";
+// alert(message);
+
+// const userPassword = "jqueryismyjam";
+
+// const ADMIN_PASSWORD = "jqueryismyjam";
+
+// const CANCELED_BY_USER = "Отменено пользователем!";
+// const ACCESS_IS_ALLOWED = "Добро пожаловать!";
+// const ACCESS_DENIED = "Доступ запрещен, неверный пароль!";
+// let message;
+
+// // Write code under this line
+
+// // console.log(message);
+// if ((userPassword = ADMIN_PASSWOR)) {
+//   console.log(ACCESS_IS_ALLOWED);
+// }
+// //если userPassword равно 'jqueryismyjam'
+// // то значение message будет равно
+// // 'Добро пожаловать!'
+// if ((userPassword = null)) {
+//   console.log(CANCELED_BY_USER);
+// }
+// //если userPassword равно  null
+// // то значение message будет равно
+// // 'Отменено пользователем!'
+// if ((userPassword = 123)) {
+//   console.log(ACCESS_DENIED);
+// }
+// //если userPassword равно '123'
+// // то значение message будет равно
+// // 'Доступ запрещен, неверный пароль!'
